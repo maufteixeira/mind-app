3e5923a3b5b2dddd30fd865a7b28db87b25a0661
Mauricio Teixeira <maufteixeira@gmail.com>
2021-08-28 23:07:31 -0300

    Adiciona teste para modelo Tag e ajusta teste Home

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :test do
   gem 'rspec-rails'
   gem 'capybara', '>= 3.26'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
 
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,8 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -37,6 +35,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
